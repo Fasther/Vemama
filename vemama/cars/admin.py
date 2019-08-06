919ac8a0984fc577d42e1745ac37d094cb7e1919
Fasther <30392053+Fasther@users.noreply.github.com>
2019-08-06 12:42:41 +0200

    Cars model

--- vemama/cars/admin.py
+++ vemama/cars/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import City, Car
 
-# Register your models here.
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = ["car_name",
+                    "car_id",
+                    "car_city",
+                    "car_next_inspection_date",
+                    "car_next_inspection_km",
+                    "car_next_oil_date",
+                    "car_next_oil_km",
+                    "car_last_check",
+                    "car_note",
+                    "car_next_date"
+                    ]
+    readonly_fields = ["car_next_date"]
+
+
+admin.site.register(City)
+admin.site.register(Car, CarAdmin)
--- vemama/cars/models.py
+++ vemama/cars/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class City(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Car(models.Model):
+    car_name = models.CharField(max_length=200, unique=True)
+    car_id = models.DecimalField(unique=True, decimal_places=0, max_digits=5)
+    car_city = models.ForeignKey(City, related_name="city", on_delete=models.CASCADE)
+    car_next_inspection_date = models.DateField()
+    car_next_inspection_km = models.DecimalField(decimal_places=0, max_digits=10)
+    car_next_oil_date = models.DateField()
+    car_next_oil_km = models.DecimalField(decimal_places=0, max_digits=10)
+    car_last_check = models.DateTimeField()
+    car_note = models.CharField(blank=True, max_length=1024)
+
+    def next_oil_or_inspection_date(self):
+        return min(self.car_next_inspection_date, self.car_next_oil_date) if self.car_id else "0"
+
+    # def next_oil_or_inspection_km(self):
+    #     return min(self.car_next_inspection_km, self.car_next_oil_km)
+
+    def __str__(self):
+        return self.car_name
+
+    car_next_date = property(next_oil_or_inspection_date)
