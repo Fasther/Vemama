6a9475d04b3bb2dc92a5560a135776a9f7ecdd0a
Fasther <30392053+Fasther@users.noreply.github.com>
2019-08-21 22:37:18 +0200

    Tasks model + admin reg

--- vemama/tasks/admin.py
+++ vemama/tasks/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ["name",
+                    "car",
+                    "user",
+                    "due_date",
+                    "completed_date",
+
+    ]
+    readonly_fields = ["created_date"]
+    raw_id_fields = ["car"]
+
+
+admin.site.register(Task, TaskAdmin)
--- vemama/tasks/models.py
+++ vemama/tasks/models.py
@@ -5,11 +5,12 @@ from django.contrib.auth.models import User
 
 class Task(models.Model):
     name = models.CharField(max_length=200)
-    car = models.ForeignKey(cars.Car)
-    user = models.ForeignKey(User)
+    car = models.ForeignKey(cars.Car, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     description = models.TextField(blank=True)
     created_date = models.DateField(auto_now_add=True)
-    due_date = models.DateField(blank=True)
-    completed_date = models.DateField(blank=True)
+    due_date = models.DateField(blank=True, null=True)
+    completed_date = models.DateField(blank=True, null=True)
 
-# TODO: str, done,
+    def __str__(self):
+        return self.name
