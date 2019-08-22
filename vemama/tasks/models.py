8a65ee252b81936b650e5f368b285615c0720651
Fasther <30392053+Fasther@users.noreply.github.com>
2019-08-22 12:12:39 +0200

    Tasks model

--- vemama/tasks/models.py
+++ vemama/tasks/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class Task(models.Model):
     name = models.CharField(max_length=200)
     car = models.ForeignKey(cars.Car, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
     description = models.TextField(blank=True)
     created_date = models.DateField(auto_now_add=True)
     due_date = models.DateField(blank=True, null=True)
