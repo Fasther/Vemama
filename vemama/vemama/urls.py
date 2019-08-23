111169a470cc854af85dc9eb950215bea5bed61a
Fasther <30392053+Fasther@users.noreply.github.com>
2019-08-23 16:32:28 +0200

    login form

--- vemama/vemama/urls.py
+++ vemama/vemama/urls.py
@@ -19,5 +19,5 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
+    path("", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
 ]
