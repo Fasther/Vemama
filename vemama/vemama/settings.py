cf16ce24c833302a65bfceae66522dbe0b09112b
Fasther <30392053+Fasther@users.noreply.github.com>
2019-08-23 13:13:08 +0200

    Crispy settings

--- vemama/vemama/settings.py
+++ vemama/vemama/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "crispy_forms"
     "cars",
     "tasks"
 ]
@@ -122,6 +123,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
