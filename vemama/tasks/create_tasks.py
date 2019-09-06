3ce770f5617bf558d2e96459b5ba18a43c7565b4
Fasther <30392053+Fasther@users.noreply.github.com>
2019-09-06 18:47:21 +0200

    Create tasks - service will be soon

--- vemama/tasks/create_tasks.py
+++ vemama/tasks/create_tasks.py
@@ -4,16 +4,29 @@ from datetime import timedelta
 from django.utils import timezone
 
 
-def create_check_tasks():
+def tasks_already_exist(car, task_name):
+    tasks = car.tasks.all()
+    for task in tasks:
+        if task.name == task_name:
+            return True
+    return False
+
+
+def create_service_tasks():
+    task_name = "Regular service inspection will be soon"  # service tasks name that will be used
     cars = carsmodels.Car.objects.all()
     for car in cars:
         car_needs_service = False
         # check for date - service in less than 30 days
-        if timedelta(30) > (car.next_oil_or_inspection_date() - timezone.now().date()):
+        if timedelta(30) < (car.next_oil_or_inspection_date() - timezone.now().date()):
             car_needs_service = True
+        print(str(car) + str(car_needs_service))
         # check for kms
         if car.next_oil_or_inspection_kms() < 3000:
             car_needs_service = True
-
+        print(str(car) + str(car_needs_service))
+        if car_needs_service:
+            if not tasks_already_exist(car, task_name): # if task is not existing...
+                print(str(car) + "have no task of that name")
 
 
