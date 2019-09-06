6760d2d364a9f5be4be207cc6a40d60e5cf1daf4
Fasther <30392053+Fasther@users.noreply.github.com>
2019-09-06 19:47:15 +0200

    Removed debugging prints

--- vemama/tasks/create_tasks.py
+++ vemama/tasks/create_tasks.py
@@ -20,19 +20,18 @@ def create_task(car, task_name):
 def create_service_tasks():
     task_name = "Regular service inspection soon"  # service tasks name that will be used
     cars = carsmodels.Car.objects.all()
+    tasks_created = 0
     for car in cars:
         car_needs_service = False
         # check for date - service in less than 30 days
         if timedelta(30) < (car.next_oil_or_inspection_date() - timezone.now().date()):
             car_needs_service = True
-        print(str(car) + str(car_needs_service))
         # check for kms
         if car.next_oil_or_inspection_kms() < 3000:
             car_needs_service = True
-        print(str(car) + str(car_needs_service))
         if car_needs_service:
             if not tasks_already_exist(car, task_name): # if task is not existing...
-                print(str(car) + "have no task of that name")
                 create_task(car, task_name)
+                tasks_created += 1
 
 
