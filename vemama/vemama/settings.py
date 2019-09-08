2c0186b811572566263bd73b4158475524aea56c
Fasther <30392053+Fasther@users.noreply.github.com>
2019-09-08 21:12:04 +0200

    disabled debug toolbar

--- vemama/vemama/settings.py
+++ vemama/vemama/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "debug_toolbar",
     "crispy_forms",
     "cars",
     "tasks",
@@ -60,7 +59,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'vemama.urls'
