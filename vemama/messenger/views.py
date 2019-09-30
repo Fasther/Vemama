587f6d02d52ca5744a9db07c68f6c1fb8fa19e5a
Fasther <30392053+Fasther@users.noreply.github.com>
2019-09-30 18:53:34 +0200

    Messenger app - echo test

--- vemama/messenger/views.py
+++ vemama/messenger/views.py
@@ -17,7 +17,7 @@ def post_facebook_message(fbid, recevied_message):
     post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token={}'.format(PAGE_ACCESS_TOKEN)
     response_msg = json.dumps({"recipient": {"id": fbid}, "message": {"text": recevied_message}})
     status = requests.post(post_message_url, headers={"Content-Type": "application/json"}, data=response_msg)
-    pprint(status.json())
+    pprint("--- odpověď ---", status.json())
 
 
 class MessengerView(generic.View):
