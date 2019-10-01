5ffb9b5da84b8a83d5bfae98a8db4244a11b6fd9
Fasther <30392053+Fasther@users.noreply.github.com>
2019-10-01 21:24:46 +0200

    Messenger app - canceled

--- vemama/messenger/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "messenger"
-
-urlpatterns = [
-    path("1d03e747abc9832543417431adb24fac56be777df43ff2d619", views.MessengerView.as_view(), name="messenger"),
-    path("send", views.send_message_view, name="sample_message"),
-]
