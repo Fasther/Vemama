734d4515efc68ac0dcc27996b26f47e953dd7020
Fasther <30392053+Fasther@users.noreply.github.com>
2019-10-03 23:42:54 +0200

    Show My tasks with total and overdue tasks count on main page

--- vemama/vemama/settings.py
+++ vemama/vemama/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'kbj5e+y=b85!-b9w7zjrq4mc+h-)v(ho3w!(u-h=15hdeqzkb('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["ed564dca.ngrok.io", ]
+ALLOWED_HOSTS = ["ed564dca.ngrok.io", "127.0.0.1", ]
 
 INTERNAL_IPS = [
     '127.0.0.1',
--- vemama/vemama/views.py
+++ vemama/vemama/views.py
@@ -1,1 +1,14 @@
 from django.views.generic import TemplateView
+from tasks.models import Task
+from django.utils import timezone
+
+
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks_num'] = Task.objects.filter(user=self.request.user, completed=False).count()
+        context['tasks_num_over'] = Task.objects.filter(user=self.request.user, completed=False,
+                                                        due_date__lte=timezone.now()).count()
+        return context
