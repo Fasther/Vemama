c839c4885089e4f6287a50545f94de3718fdd24a
Fasther <30392053+Fasther@users.noreply.github.com>
2019-11-10 14:40:18 +0100

    Assign tasks - fixed few bugs (no tasks to assign)

--- vemama/tasks/assign_tasks.py
+++ vemama/tasks/assign_tasks.py
@@ -22,21 +22,23 @@ def get_tasks(tasks_name):
 # Assign tasks based on city
 
 def assign_tasks(tasks: dict):
-    task_type = " worker" if next(iter(tasks.items()))[1][0].name == "Routine check" else " fleet"
+    if not tasks:
+        return 0
+    task_type = " fleet" if next(iter(tasks.items()))[1][0].name == inspection_name else " worker"
+    tasks_assigned = 0
     for city in tasks:
         group_name = city + task_type
         users = get_user_model().objects.filter(groups__name=group_name)
         users_counts = {}  # get user counts to assign task to user with lowest tasks count
         for user in users:
             users_counts[user] = user.tasks.filter(completed=False).count()
-        tasks_assigned = 0
         for task in tasks[city]:
             user = min(users_counts, key=users_counts.get)
             task.user = user
             task.save()
             users_counts[user] += 1
             tasks_assigned += 1
-        return tasks_assigned
+    return tasks_assigned
 
 
 def assign_check_inspection_tasks():  # main function returning results to view
