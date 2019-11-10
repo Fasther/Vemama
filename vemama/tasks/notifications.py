b881d4b6d33211592f5d6a498fc3a3c80f78d704
Fasther <30392053+Fasther@users.noreply.github.com>
2019-11-10 18:47:31 +0100

    Notifications email - dont send if no tasks to be sent

--- vemama/tasks/notifications.py
+++ vemama/tasks/notifications.py
@@ -6,13 +6,14 @@ from datetime import timedelta
 
 
 def send_notification(heading, tasks):
-    email = tasks[0].user.email
-    html_message = loader.render_to_string(
-        'tasks/notif_email_template.html',
-        {"heading": heading, 'tasks': tasks}
-    )
-    send_mail(heading, "", "'Vemama' <vemama@pancho.cz>", [email, ], fail_silently=False,
-              html_message=html_message)
+    if tasks:
+        email = tasks[0].user.email
+        html_message = loader.render_to_string(
+            'tasks/notif_email_template.html',
+            {"heading": heading, 'tasks': tasks}
+        )
+        send_mail(heading, "", "'Vemama' <vemama@pancho.cz>", [email, ], fail_silently=False,
+                  html_message=html_message)
     return "Done"
 
 
