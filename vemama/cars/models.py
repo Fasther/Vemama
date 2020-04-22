b2d27feaff6a502887fa88deb97722eefc460173
Fasther <30392053+Fasther@users.noreply.github.com>
2020-04-22 22:28:04 +0200

    Added Tyres, STK, Need of wet cleaning to Car

--- vemama/cars/models.py
+++ vemama/cars/models.py
@@ -17,19 +17,34 @@ class City(models.Model):
 
 
 class Car(models.Model):
+    CAR_TYRES = (
+        (1, "All year"),
+        (2, "Summer"),
+        (3, "Winter")
+    )
+    DIRTINESS = (
+        (1, "Fine, looks good"),
+        (2, "Few stains, still good enough"),
+        (3, "Car needs wet cleaning. Really.")
+    )
+    is_active = models.BooleanField(default=True)
     car_name = models.CharField(max_length=200, unique=True)
     car_id = models.DecimalField(unique=True, decimal_places=0, max_digits=5)
     car_city = models.ForeignKey(City, related_name="cars", on_delete=models.CASCADE, verbose_name="City")
+    car_tyres = models.IntegerField(choices=CAR_TYRES, blank=True, null=True, default=1)
+    car_dirtiness = models.IntegerField(choices=DIRTINESS, blank=True, null=True, default=1)
     car_next_inspection_date = models.DateField()
     car_next_inspection_km = models.DecimalField(decimal_places=0, max_digits=10)
     car_next_oil_date = models.DateField()
     car_next_oil_km = models.DecimalField(decimal_places=0, max_digits=10)
-    car_last_check = models.DateField()
+    car_next_stk_date = models.DateField(blank=True, null=True)
+    car_last_check = models.DateField(blank=True, null=True)
     car_actual_driven_kms = models.DecimalField(decimal_places=0, max_digits=10)
     car_note = models.TextField(blank=True, max_length=1024)
 
     def next_oil_or_inspection_date(self):
         return min(self.car_next_inspection_date, self.car_next_oil_date) if self.car_id else "0"
+
     next_oil_or_inspection_date.short_description = "Next 🔧 date"
 
     def next_oil_or_inspection_kms(self):
