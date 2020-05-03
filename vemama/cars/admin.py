2d1c9c78083049368807d598a060473f7710f62f
Fasther <30392053+Fasther@users.noreply.github.com>
2020-05-03 10:11:40 +0200

    Implemented history for models

--- vemama/cars/admin.py
+++ vemama/cars/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
+from simple_history.admin import SimpleHistoryAdmin
 from .models import City, Car
 
 
-class CarAdmin(admin.ModelAdmin):
+class CarAdmin(SimpleHistoryAdmin):
     list_display = ["car_name",
                     "car_id",
                     "car_city",
@@ -12,12 +13,19 @@ class CarAdmin(admin.ModelAdmin):
                     "car_next_date",
                     ]
     list_editable = ["car_actual_driven_kms", ]
+    history_list_display = ["car_actual_driven_kms", ]
     readonly_fields = ["car_next_date",
                        "car_next_km",
                        ]
     list_filter = ["car_city"]
     search_fields = ["car_name"]
 
+    @staticmethod
+    def changed_fields(obj):
+        if obj.prev_record:
+            delta = obj.diff_against(obj.prev_record)
+            return delta.changed_fields
+        return None
 
 admin.site.register(City)
 admin.site.register(Car, CarAdmin)
--- vemama/tasks/admin.py
+++ vemama/tasks/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
+from simple_history.admin import SimpleHistoryAdmin
 from .models import Task
 
 
-class TaskAdmin(admin.ModelAdmin):
+class TaskAdmin(SimpleHistoryAdmin):
     list_display = ["name",
                     "car",
                     "user",
@@ -12,8 +13,16 @@ class TaskAdmin(admin.ModelAdmin):
                     "completed_date",
                     ]
     readonly_fields = ["created_date", "completed", "is_past_due", "city", ]
+    history_list_display = ["completed", "changed_fields"]
     raw_id_fields = ["car", ]
     list_filter = ["city", "completed", "user"]
+
+    @staticmethod
+    def changed_fields(obj):
+        if obj.prev_record:
+            delta = obj.diff_against(obj.prev_record)
+            return delta.changed_fields
+        return None
 
 
 admin.site.register(Task, TaskAdmin)
