4524e85a9ff81e5e5256b0374964957b56b6d4f3
Fasther <30392053+Fasther@users.noreply.github.com>
2020-06-08 07:55:04 +0200

    Admin tweaks

--- vemama/core/admin.py
+++ vemama/core/admin.py
@@ -8,11 +8,26 @@ from core.models import Profile, Person
 class ProfileAdmin(admin.ModelAdmin):
     autocomplete_fields = ("user",)
     search_fields = ("user",)
+    list_display = ("user", "cities_list", "active_tasks")
+
+    def get_queryset(self, request):
+        query_set = super().get_queryset(request)
+        return query_set.filter(user__is_active=True)
+
+    def cities_list(self, obj):
+        return ", ".join(str(city) for city in obj.cities.all())
+
+    cities_list.short_description = "Cities"
+
+    def active_tasks(self, obj):
+        return len(obj.user.tasks.filter(completed=False))
 
 
 @admin.register(Person)
-class ProfileAdmin(admin.ModelAdmin):
+class PersonAdmin(admin.ModelAdmin):
     search_fields = ("username", "first_name", "last_name")
+    list_display = ("__str__", "email", "is_active", "is_staff")
+    list_filter = ("is_active",)
 
 
 admin.site.unregister(User)
