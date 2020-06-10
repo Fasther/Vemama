8f43d13dd717152b29e0593b3dbd59792772ca1c
Fasther <30392053+Fasther@users.noreply.github.com>
2020-06-10 20:45:57 +0200

    Create report task

--- vemama/tasks/forms.py
+++ vemama/tasks/forms.py
@@ -10,3 +10,16 @@ class CompleteTaskForm(ModelForm):
     class Meta:
         model = Task
         fields = ("completed_date",)
+
+
+class CreateReportTask(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["task_type"].choices = (
+            (1, "Regular cleaning and check"),
+            (99, "Other"),
+        )
+
+    class Meta:
+        model = Task
+        fields = ("name", "task_type", "description")
