e8c9054747d1f93f4c11fce5686f8fcb02d4676c
Pavel Pančocha <30392053+Fasther@users.noreply.github.com>
2020-06-25 10:01:39 +0200

    Show changed fields in car admin

--- vemama/cars/admin.py
+++ vemama/cars/admin.py
@@ -15,7 +15,7 @@ class CarAdmin(SimpleHistoryAdmin):
                     "car_next_stk_date",
                     ]
     list_editable = ["car_actual_driven_kms", ]
-    history_list_display = ["car_actual_driven_kms", ]
+    history_list_display = ["car_actual_driven_kms", "changed_fields", ]
     readonly_fields = ["car_next_date",
                        "car_next_km",
                        ]
